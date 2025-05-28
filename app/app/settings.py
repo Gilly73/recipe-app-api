97733fd17b8a6864651fb7c877a2b26699ddbc32
Seeta Gill <seeta.gill@gmail.com>
2025-05-28 16:11:42 +0100

    line length fix

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)#p&&heb505i$#k+8j3(w50x^6rca#qdmp1r0492_ylj=j4v@w'
+SECRET_KEY = 'django-insecure\
+    -)#p&&heb505i$#k+8j3(w50x^6rca#qdmp1r0492_ylj=j4v@w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
