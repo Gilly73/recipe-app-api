67a364da12214e0fb101dc892469780764680dcc
Seeta Gill <seeta.gill@gmail.com>
2025-06-02 13:48:54 +0100

    added admin modle with config and test

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,58 @@
+""""
+Database models for the core app."""
+
 from django.db import models # noqa
 
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+    # ** means we can pass any number of keyword arguments to the method
+    # This is useful when you define additional fields, for example, a name,
+    # you can pass it as an extra field and that will be automatically be created
+    # when the user model is created.
+    # And it just gives you a bit of flexibility when it comes to creating different
+    # fields in the user. It means any time you add new fields here,
+    # you don't need to update the create user method. You can just automatically
+    # provide them when you're calling the Create User method and it will automatically
+    # pass any values you give to the new model
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email address.')
+        # self.model is a reference to the User model clas defined below
+        # user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        # using self._db allows us to specify which database to use
+        user.save(using=self._db)
+
+        return user
+
+
+    def create_superuser(self, email, password):
+            """Create and return a new superuser."""
+            user = self.create_user(email, password)
+            user.is_staff = True
+            user.is_superuser = True
+            user.save(using=self._db)
+
+            return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,56 @@
+"""
+Tests for the Django admin modifications.
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+
+class AdminSiteTests(TestCase):
+    """Tests for Django admin."""
+
+    def setUp(self):
+        """Create user and client."""
+        # test client is used to simulate a user interacting with the application
+        # It allows us to make requests to the application and check the responses
+        # The client is used to make requests to the application and check the responses
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@example.com',
+            password='testpass123',
+        )
+        # force_login is used to simulate a user logging in
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='user@example.com',
+            password='testpass123',
+            name='Test User'
+        )
+
+    def test_users_lists(self):
+        """Test that users are listed on page."""
+        # gets reverse URL for the admin user change list
+        # reverse is used to get the URL for the admin user change list
+        # 'admin:core_user_changelist' is the name of the URL pattern for the user change list
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+
+    def test_edit_user_page(self):
+        """Test the edit user page works."""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+
+    def test_create_user_page(self):
+        """Test the create user page works."""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
